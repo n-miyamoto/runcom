dab098e523793d5ba2a9594ef0d26b65d4fd812e
n-miyamoto <miyamoto@sensprout.com>
2017-09-04 16:54:58 +0900

    add setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ ! -n "$HOME" ]; then
+	echo "HOME is undefined"
+	echo $HOME
+	exit 1
+fi
+
+#Copy all .rc files to home directory.
+for file in `\find . -name '*rc' ` ; do
+	echo $file "$HOME"
+	cp $file "$HOME"
+done
+
+exit 0
