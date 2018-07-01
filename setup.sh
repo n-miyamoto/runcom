991438c3ae03b361e620fa43bbc781bfec8b8612
n-miyamoto <scotsdowneroad@gmail.com>
2018-07-01 11:32:35 +0900

    add tmux settings

--- setup.sh
+++ setup.sh
@@ -6,10 +6,17 @@ if [ ! -n "$HOME" ]; then
 	exit 1
 fi
 
-#Copy all .rc files to home directory.
+#Copy all rc files to home directory.
 for file in `\find . -name '*rc' ` ; do
 	echo $file "$HOME"
 	cp $file "$HOME"
 done
+
+#Copy all .conf files to home directory.
+for file in `\find . -name '*conf' ` ; do
+	echo $file "$HOME"
+	cp $file "$HOME"
+done
+
 
 exit 0
